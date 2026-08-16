f9f4711843d050e3fbce5a4e58e8d0f6a38b8fd3
Sheras-art <s32742534@gmail.com>
2026-08-16 06:01:54 +0500

    fix: standardize basename and update visual styles in Hero component

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/rova-one">
+    <BrowserRouter basename="/ROVA-one">
       <App />
     </BrowserRouter>
   </StrictMode>,
